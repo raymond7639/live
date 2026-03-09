ed7ec3755a9e25327df97794c14c22400ea153d6
Raymond <vishal1.pdf@gmail.com>
2026-03-10 00:24:28 +0530

    day 14 done

---
+++ day-15/script.js
@@ -0,0 +1,13 @@
+// console.log("hi")
+// console.warn("warning")
+// console.error("Error 404")
+// alert - for message
+// confirm - to confirm yes/no
+// prompt - write answer
+// alert("are u sure ?")
+// confirm("are u there ?")
+
+
+
+var a =prompt("Write your name ")
+console.log(a)
