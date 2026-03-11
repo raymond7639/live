3b36951a02115fba5db68acdff5d5048128d1ca8
Raymond <vishal1.pdf@gmail.com>
2026-03-12 01:50:50 +0530

    march 11

---
+++ day-18/script.js
@@ -0,0 +1,12 @@
+// function this_is_fun(a,b,c,...ahaa){
+//     console.log(a,b,c,ahaa)
+// }
+
+
+// var a = 10;
+// console.log(a);
+
+// IIFE : immediately invoked function expression
+(function () {
+ 
+})
