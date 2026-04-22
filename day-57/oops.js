3cd99ba4dbc7e6d8cc684792fabea7a9d21ed92d
Raymond <vishal1.pdf@gmail.com>
2026-04-23 01:28:51 +0530

    cmt

--- day-57/oops.js
+++ day-57/oops.js
@@ -1,7 +1,11 @@
 class Bottle{
-    constructor(){
-        this.company= 'Milton';
-        this.price = 700;
-        this.review = 4;
+    constructor(bottle_name,price,review){
+        this.company= bottle_name;
+        this.price = price;
+        this.review = review;
     }
 }
+
+
+var bot1 = new Bottle("Pexpo",1000,5);
+var bot2 = new Bottle("Milton",129,3);
