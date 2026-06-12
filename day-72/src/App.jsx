d0bbcd3747433f107ceba257a4b6d38bbd160368
Raymond <vishal1.pdf@gmail.com>
2026-06-12 21:08:08 +0530

    push

--- day-72/src/App.jsx
+++ day-72/src/App.jsx
@@ -1,17 +1,13 @@
 const App = () => {
-  const [users, setusers] = useState([
-    { name: "vihsja;", age: 21 },
-    { name: "Noone;", age: 1 },
-    { name: "Bone;", age: 11 },
-  ])
-  const renderUser = users.map((use, ind) => {
-    return <li key={ind}>{user.name}</li>
-  })
+
+
 
   return (
     <div>
-      <h1>Users</h1>
-      <ol>{renderUser}</ol>
+      <form onChange={} action="">
+        <input type="text" name="" id="" />
+
+      </form>
     </div>
 
   );
