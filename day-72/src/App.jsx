41170840b966ed0dff1aed5b9c6a1f7814232de3
Raymond <vishal1.pdf@gmail.com>
2026-06-12 20:51:31 +0530

    pus

--- day-72/src/App.jsx
+++ day-72/src/App.jsx
@@ -1,7 +1,19 @@
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [users, setusers] = useState([
+    { name: "vihsja;", age: 21 },
+    { name: "Noone;", age: 1 },
+    { name: "Bone;", age: 11 },
+  ])
+  const renderUser = users.map((use, ind) => {
+    return <li key={ind}>{user.name}</li>
+  })
 
-export default App
+  return (
+    <div>
+      <h1>Users</h1>
+      <ol>{renderUser}</ol>
+    </div>
+
+  );
+};
+export default App;
