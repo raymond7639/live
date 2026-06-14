cc21694064c03d2755219e23761b912d94dd93c3
Raymond <vishal1.pdf@gmail.com>
2026-06-14 23:52:40 +0530

    commiting

--- day-75/src/App.jsx
+++ day-75/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+// import style from "./App.module.css"
 const App = () => {
   const [name, setname] = useState("");
 
   return (
     <div>
       <form>
+        <h1 className="app_h1">Forms</h1>
         <input type="text" onChange={(e)=>setname(e.target.value)} value={name} placeholder="Your Name"/><br />
         <input type="radio" /><br />
         <button>Submit</button>
